e81beb3a9584b66e321dcc61bec5f4fc3d5e94c7
Miguel Samori de Azevedo <74837975+cmiguel12@users.noreply.github.com>
2024-04-17 14:38:03 +0000

    Atualização Aula

--- index.js
+++ index.js
@@ -5,13 +5,31 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-// Exercicio: Criar um endpoint [GET] /oi que exibe: " Olá mundo!"
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// Exercício: Criar um endpoint [GET] /oi que exibe: "Olá, mundo!"
 app.get('/oi', function (req, res) {
-    res.send('Olá, mundo')
-  })
+  res.send('Olá, mundo!')
+})
+
+// Lista de Itens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+//              0               1              2
+
+// Endpoint Read All -> [GET] /item
+app.get('/item', function (req, res) {
+  res.send(lista)
+})
+
+// Endpoint Read By ID -> [GET] /item/:id
+app.get('/item/:id', function (req, res) {
+  // Acessamos o parâmetro de rota ID
+  const id = req.params.id
+
+  // Acessamos o item na lista (usando o id - 1)
+  // e colocamos na variável item
+  const item = lista[id - 1]
+
+  // Enviamos para a resposta o item acessado
+  res.send(item)
+})
+
 app.listen(3000)
